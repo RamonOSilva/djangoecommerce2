340efdf637c0dc2765470e9cc336e80a2265b9f2
Ramon Silva <ramondeosilva@gmail.com>
2018-11-09 10:05:58 -0300

    teste

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = '7hs2aw-9x=u9l^wsfvovy(%52k4vj9rdv5gk@_sp(l41w=noe$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                
+
                 # apps
                 'catalog.context_processors.categories',
             ],
