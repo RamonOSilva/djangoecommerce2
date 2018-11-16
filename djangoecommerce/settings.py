71b40fbf373c7506a3e3aecb8be4cb973eb13a71
Ramon Silva <ramondeosilva@gmail.com>
2018-11-16 14:45:25 -0300

    inserindo pypal como sistema de pagamento

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # libs
     'widget_tweaks',
+    'paypal.standard.ipn',
     # apps
     'core',
     'accounts',
@@ -174,6 +175,9 @@ MESSAGE_TAGS = {
 PAGSEGURO_TOKEN = '3889EDB1CA8F43A0A2D67A892DB514DD'
 PAGSEGURO_EMAIL = 'ramondeosilva@gmail.com'
 PAGSEGURO_SANDBOX = True
+
+PAYPAL_TEST = True
+PAYPAL_EMAIL = 'ramondeosilva@gmail.com'
 
 try:
     from .local_settings import *
