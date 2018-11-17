a976330a1f04a8da958e5266d342e2940e2e1187
Ramon Silva <ramondeosilva@gmail.com>
2018-11-17 16:58:38 -0300

    adicionando cache do produto

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -192,6 +192,14 @@ THUMBNAIL_ALIASES = {
     },
 }
 
+# cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'cache',
+    }
+}
+
 try:
     from .local_settings import *
 except ImportError:
